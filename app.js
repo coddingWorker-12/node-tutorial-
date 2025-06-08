6a2fe9fc983d2f9dfcc47f42dfbd10990d2f2873
coddingWorker-12 <kumarashok15957@gmail.com>
2025-06-08 18:27:01 +0530

    node js import export project

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports = {
+    x:"20",
+    y:"15",
+    z:function(a,b){
+        console.log(a+b)
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('./app')
+
+
+let a = 10;
+let b = 20;
+app.z(10,12);
