24b5512cf1d4be26a956218b748f5e0dfe7ff8f1
coddingWorker-12 <kumarashok15957@gmail.com>
2025-06-09 18:37:59 +0530

    create and delet file from gating input from terminal using process.argv

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const http = require('http');
-const data = require('./app.js');
+const fs = require('fs');
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'application/json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(5000);
+let input = process.argv;
+
+if(input[2]=='add'){
+   fs.writeFileSync(input[3],input[4]);
+}else if(input[2]=='remove'){
+   fs.unlinkSync(input[3]);
+}
+else{
+    console.log("invalid input");
+}
