c60a18bc5853cf946ce59a9b54b9c9d85c93814a
coddingWorker-12 <kumarashok15957@gmail.com>
2025-06-10 16:23:48 +0530

    file list && use path modules

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+let pathname = path.join(__dirname,'files');
+console.log(pathname);
 
-let input = process.argv;
+// for(i=0;i<5;i++){
+//    fs.writeFileSync(pathname+`/hello${i}.txt`,'hello its a trile file');
+// }
 
-if(input[2]=='add'){
-   fs.writeFileSync(input[3],input[4]);
-}else if(input[2]=='remove'){
-   fs.unlinkSync(input[3]);
-}
-else{
-    console.log("invalid input");
-}
+fs.readdir(pathname,(err,files)=>{
+   files.forEach((item)=>{
+      console.log('your file is :',item);
+   })
+})
